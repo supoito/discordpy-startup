2f1559a4318c7a8ba025e7c983df3726ea463e3b
supoito <84701223+supoito@users.noreply.github.com>
2021-05-24 20:56:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,28 @@
-from discord.ext import commands
-import os
-import traceback
+import gspread
+import json
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+#ServiceAccountCredentials：Googleの各サービスへアクセスできるservice変数を生成します。
+from oauth2client.service_account import ServiceAccountCredentials 
 
+#2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+#認証情報設定
+#ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
+credentials = ServiceAccountCredentials.from_json_keyfile_name('asarikokuti-6bb0166725e7.json', scope)
 
+#OAuth2の資格情報を使用してGoogle APIにログインします。
+gc = gspread.authorize(credentials)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+#共有設定したスプレッドシートキーを変数[SPREADSHEET_KEY]に格納する。
+SPREADSHEET_KEY = '1p5NK1hgzI17CtnI8-CQqC7GzRMhwWgT3zc41S_N3zpw'
 
+#共有設定したスプレッドシートのシート1を開く
+worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
 
-bot.run(token)
+#A1セルの値を受け取る
+import_value = int(worksheet.acell('A1').value)
+
+#A1セルの値に100加算した値をB1セルに表示させる
+export_value = import_value+100
+worksheet.update_cell(1,2, export_value)
